b446225f114f5035c78bce95fd2baa9ce4228f59
Dan Greco <me@dangre.co>
2025-06-11 17:00:04 -0400

    Add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	injectSpeedInsights();
+	injectAnalytics();
 
 	let { children } = $props();
 </script>
